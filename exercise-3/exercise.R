1a3c9a126504b35004c1398c1bddab3236248a67
Maggie Tsang <mbytsang@gmail.com>
2017-04-11 14:30:39 -0700

    Half exercise 3

--- exercise-3/exercise.R
+++ exercise-3/exercise.R
@@ -1,26 +1,26 @@
 # Exercise 3: Working with Data Frames
 
 # Load R's "USPersonalExpenditure" dataest using the `data()` function
-
+data("USPersonalExpenditure")
 
 # The variable USPersonalExpenditure is now accessible to you. Unfortunately, it's not a data.frame
 # Test this using the is.data.frame function
-
+is.data.frame(USPersonalExpenditure)
 
 # Luckily, you can simply pass the USPersonalExpenditure variable to the data.frame function
 # to convert it a data.farme
 
 # Create a new variable by passing the USPersonalExpenditure to the data.frame function
-
+us.per.exp <- data.frame(USPersonalExpenditure)
 
 # What are the column names of your dataframe?
-
+colnames(us.per.exp)
 
 # Why are they so strange?
 
 
 # What are the row names of your dataframe?
-
+rownames(us.per.exp)
 
 # Create a column `category` that is equal to your rownames
 
